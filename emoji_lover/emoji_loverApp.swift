e5cc01b66e7eca5d0530473e9a1ac570beab8b97
claudemo <myliu.pro@gmail.com>
2023-10-04 20:04:13 -0400

    Initial Commit

---
+++ emoji_lover/emoji_loverApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_loverApp.swift
+//  emoji_lover
+//
+//  Created by Mengyue  on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct emoji_loverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
